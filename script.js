9ed252ab04c655283d7494cc06f148490cef2c3b
brianlu18 <brianlu18@g.ucla.edu>
2023-05-25 02:04:34 -0500

    finished generatePassword function

--- script.js
+++ script.js
@@ -89,6 +89,7 @@ function generatePassword() {
     alert("Must pick at least one type of character.");
     return null;
   }
+
   // generates password if user selects only special characters
   else if (
     spBool === true &&
@@ -102,6 +103,7 @@ function generatePassword() {
     }
     return passwordStr;
   }
+
   // generates password if user selects only numeric characters
   else if (
     spBool === false &&
@@ -115,6 +117,7 @@ function generatePassword() {
     }
     return passwordStr;
   }
+
   // generates password if user selects only lowercase characters
   else if (
     spBool === false &&
@@ -128,6 +131,7 @@ function generatePassword() {
     }
     return passwordStr;
   }
+
   // generates password if user selects only uppercase characters
   else if (
     spBool === false &&
@@ -140,24 +144,208 @@ function generatePassword() {
       passwordStr = passwordStr + randChar;
     }
     return passwordStr;
-  } // generates password if user selects special and numeric characters
+  }
+
+  // generates password if user selects special and numeric characters
   else if (
     spBool === true &&
     numBool === true &&
     lowerBool === false &&
     upperBool === false
   ) {
-    for (let i = 0; i < password_len; i++) {
-      passwordStr.concat(getRandomIndex(numChar));
+    var spNumArr = spChar.concat(numChar);
+    for (let i = 0; i < password_len - 2; i++) {
+      var randChar = getRandomIndex(spNumArr);
+      passwordStr = passwordStr + randChar;
     }
+    passwordStr = passwordStr + getRandomIndex(spChar);
+    passwordStr = passwordStr + getRandomIndex(numChar);
     return passwordStr;
   }
-  
-  
 
+  // generates password if user selects special and lowercase characters
+  else if (
+    spBool === true &&
+    numBool === false &&
+    lowerBool === true &&
+    upperBool === false
+  ) {
+    var spLowerArr = spChar.concat(alphaLowerChar);
+    for (let i = 0; i < password_len - 2; i++) {
+      var randChar = getRandomIndex(spLowerArr);
+      passwordStr = passwordStr + randChar;
+    }
+    passwordStr = passwordStr + getRandomIndex(spChar);
+    passwordStr = passwordStr + getRandomIndex(alphaLowerChar);
+    return passwordStr;
+  }
+
+  // generates password if user selects special and uppercase characters
+  else if (
+    spBool === true &&
+    numBool === false &&
+    lowerBool === false &&
+    upperBool === true
+  ) {
+    var spUpperArr = spChar.concat(alphaUpperChar);
+    for (let i = 0; i < password_len - 2; i++) {
+      var randChar = getRandomIndex(spUpperArr);
+      passwordStr = passwordStr + randChar;
+    }
+    passwordStr = passwordStr + getRandomIndex(spChar);
+    passwordStr = passwordStr + getRandomIndex(alphaUpperChar);
+    return passwordStr;
+  }
+
+  // generates password if user selects numeric and lowercase characters
+  else if (
+    spBool === false &&
+    numBool === true &&
+    lowerBool === true &&
+    upperBool === false
+  ) {
+    var numLowerArr = numChar.concat(alphaLowerChar);
+    for (let i = 0; i < password_len - 2; i++) {
+      var randChar = getRandomIndex(numLowerArr);
+      passwordStr = passwordStr + randChar;
+    }
+    passwordStr = passwordStr + getRandomIndex(numChar);
+    passwordStr = passwordStr + getRandomIndex(alphaLowerChar);
+    return passwordStr;
+  }
+
+  // generates password if user selects numeric and uppercase characters
+  else if (
+    spBool === true &&
+    numBool === false &&
+    lowerBool === false &&
+    upperBool === true
+  ) {
+    var numUpperArr = numChar.concat(alphaUpperChar);
+    for (let i = 0; i < password_len - 2; i++) {
+      var randChar = getRandomIndex(numUpperArr);
+      passwordStr = passwordStr + randChar;
+    }
+    passwordStr = passwordStr + getRandomIndex(numChar);
+    passwordStr = passwordStr + getRandomIndex(alphaUpperChar);
+    return passwordStr;
+  }
+
+  // generates password if user selects lowercase and uppercase characters
+  else if (
+    spBool === false &&
+    numBool === false &&
+    lowerBool === true &&
+    upperBool === true
+  ) {
+    var lowerUpperArr = upperChar.concat(alphaLowerChar);
+    for (let i = 0; i < password_len - 2; i++) {
+      var randChar = getRandomIndex(lowerUpperArr);
+      passwordStr = passwordStr + randChar;
+    }
+    passwordStr = passwordStr + getRandomIndex(alphaUpperChar);
+    passwordStr = passwordStr + getRandomIndex(alphaLowerChar);
+    return passwordStr;
+  }
+
+  // generates password if user selects special, numeric, and lowercase characters
+  else if (
+    spBool === true &&
+    numBool === true &&
+    lowerBool === true &&
+    upperBool === false
+  ) {
+    var spNumArr = spChar.concat(numChar);
+    var spNumLowerArr = spNumArr.concat(alphaLowerChar);
+    for (let i = 0; i < password_len - 3; i++) {
+      var randChar = getRandomIndex(spNumLowerArr);
+      passwordStr = passwordStr + randChar;
+    }
+    passwordStr = passwordStr + getRandomIndex(spChar);
+    passwordStr = passwordStr + getRandomIndex(alphaLowerChar);
+    passwordStr = passwordStr + getRandomIndex(numChar);
+    return passwordStr;
+  }
+
+  // generates password if user selects special, uppercase, and lowercase characters
+  else if (
+    spBool === true &&
+    numBool === false &&
+    lowerBool === true &&
+    upperBool === true
+  ) {
+    var spLowerArr = spChar.concat(alphaLowerChar);
+    var spLowerUpperArr = spLowerArr.concat(alphaUpperChar);
+    for (let i = 0; i < password_len - 3; i++) {
+      var randChar = getRandomIndex(spLowerUpperArr);
+      passwordStr = passwordStr + randChar;
+    }
+    passwordStr = passwordStr + getRandomIndex(spChar);
+    passwordStr = passwordStr + getRandomIndex(alphaLowerChar);
+    passwordStr = passwordStr + getRandomIndex(alphaUpperChar);
+    return passwordStr;
+  }
+
+  // generates password if user selects special, numeric, and uppercase characters
+  else if (
+    spBool === true &&
+    numBool === true &&
+    lowerBool === false &&
+    upperBool === true
+  ) {
+    var spNumArr = spChar.concat(numChar);
+    var spNumUpperArr = spNumArr.concat(alphaUpperChar);
+    for (let i = 0; i < password_len - 3; i++) {
+      var randChar = getRandomIndex(spNumUpperArr);
+      passwordStr = passwordStr + randChar;
+    }
+    passwordStr = passwordStr + getRandomIndex(spChar);
+    passwordStr = passwordStr + getRandomIndex(alphaUpperChar);
+    passwordStr = passwordStr + getRandomIndex(numChar);
+    return passwordStr;
+  }
+
+  // generates password if user selects uppercase, numeric, and lowercase characters
+  else if (
+    spBool === false &&
+    numBool === true &&
+    lowerBool === true &&
+    upperBool === true
+  ) {
+    var numLowerArr = numChar.concat(alphaLowerChar);
+    var numLowerUpperArr = numLowerArr.concat(alphaUpperChar);
+    for (let i = 0; i < password_len - 3; i++) {
+      var randChar = getRandomIndex(numLowerUpperArr);
+      passwordStr = passwordStr + randChar;
+    }
+    passwordStr = passwordStr + getRandomIndex(numChar);
+    passwordStr = passwordStr + getRandomIndex(alphaLowerChar);
+    passwordStr = passwordStr + getRandomIndex(alphaUpperChar);
+    return passwordStr;
+  } 
+  
+  // all character types selected
+  else if (
+    spBool === true &&
+    numBool === true &&
+    lowerBool === true &&
+    upperBool === true
+  ) {
+    
+    var numLowerArr = numChar.concat(alphaLowerChar);
+    var numLowerUpperArr = numLowerArr.concat(alphaUpperChar);
+    var allArr = numLowerUpperArr.concat(spChar); 
+    for (let i = 0; i < password_len - 3; i++) {
+      var randChar = getRandomIndex(allArr);
+      passwordStr = passwordStr + randChar;
+    }
+    passwordStr = passwordStr + getRandomIndex(spChar);
+    passwordStr = passwordStr + getRandomIndex(numChar);
+    passwordStr = passwordStr + getRandomIndex(alphaLowerChar);
+    passwordStr = passwordStr + getRandomIndex(alphaUpperChar);
+    return passwordStr;
+  } 
 }
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
